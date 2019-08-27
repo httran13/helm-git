a33a307a6fa2fadcf6e2cce97c9a097dc655fcc5
Huy Tran <httran8884@gmail.com>
2019-08-27 14:23:25 +0530

    put back

--- getfile.sh
+++ getfile.sh
@@ -3,12 +3,10 @@
 set -e
 
 URI=$@ # eg: gitlab://username/project:master/kubernetes/helm-chart
-# eg: github://username/project:master/kubernetes/helm-chart
 PROVIDER=$(echo $URI | cut -d: -f1) # eg: gitlab
-GIT_URL=$(echo $URI | cut -d: -f2)
-REPO=$(echo $URI | cut -d: -f3 | sed -e "s/\/\///") # eg: username/project
-BRANCH=$(echo $URI | cut -d: -f4 | cut -d/ -f1) # eg: master
-FILEPATH=$(echo $URI | cut -d: -f4 | sed -e "s/$BRANCH\///") # eg: kubernetes/helm-chart
+REPO=$(echo $URI | cut -d: -f2 | sed -e "s/\/\///") # eg: username/project
+BRANCH=$(echo $URI | cut -d: -f3 | cut -d/ -f1) # eg: master
+FILEPATH=$(echo $URI | cut -d: -f3 | sed -e "s/$BRANCH\///") # eg: kubernetes/helm-chart
 
 # echo $URI $REPO $BRANCH $FILEPATH >&2
 
@@ -17,7 +15,7 @@ TMPDIR="$(mktemp -d)"
 cd $TMPDIR
 
 git init --quiet
-git remote add origin git@$GIT_URL:$REPO.git
+git remote add origin git@$PROVIDER.com:$REPO.git
 git pull --depth=1 --quiet origin $BRANCH
 
 if [ -f $FILEPATH ]; then # if a file named $FILEPATH exists
